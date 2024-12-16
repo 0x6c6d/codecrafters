0be63603b017025206e43f2560eb52a48334f8f8
0x6c6d <lucas_menke@icloud.com>
2024-12-16 20:43:41 +0100

    > full shell code until challenge 6

--- codecrafters-shell-cpp/src/main.cpp
+++ codecrafters-shell-cpp/src/main.cpp
@@ -1,3 +1,78 @@
+#include <iostream>
+#include <cstdlib> // for getenv
+#include <string>
+#include <vector>
+#include <sstream>
+#include <filesystem> // for type command
+
+int main()
+{
+  // program is called like this: PATH="/usr/bin:/usr/local/bin" ./your_program.sh
+  // custom PATH is set as environmental variable of the process that runs the shell
+  // env variable can be accessed via getenv()
+  const char *path = std::getenv("PATH");
+  std::vector<std::string> paths;
+
+  // split PATH by ":" to get a list of paths
+  if (path != nullptr)
+  {
+    std::string pathStr(path);
+    std::stringstream ss(pathStr);
+    std::string segment;
+    while (std::getline(ss, segment, ':'))
+    {
+      paths.push_back(segment);
+    }
+  }
+
+  // Flush after every std::cout / std:cerr
+  std::cout << std::unitbuf;
+  std::cerr << std::unitbuf;
+
+  // REPL (read-eval-print loop)
+  while (true)
+  {
+    std::cout << "$ ";
+    std::string input;
+    std::getline(std::cin, input);
+
+    // handle exit command (exit or exit 0)
+    if (input == "exit" || input == "exit 0")
+    {
+      return 0;
+    }
+
+    // handle echo command
+    const std::string echo = "echo ";
+    if (input.find(echo) == 0)
+    {
+      std::string userInput = input.substr(echo.length());
+      std::cout << userInput << std::endl;
+      continue;
+    }
+
+    // handle type command
+    const std::string type = "type ";
+    if (input.find(type) == 0)
+    {
+      std::string userInput = input.substr(type.length());
+
+      if (userInput == "echo")
+      {
+        std::cout << userInput << " is a shell builtin" << std::endl;
+        continue;
+      }
+
+      if (userInput == "exit")
+      {
+        std::cout << userInput << " is a shell builtin" << std::endl;
+        continue;
+      }
+
+      if (userInput == "type")
+      {
+        std::cout << userInput << " is a shell builtin" << std::endl;
+        continue;
       }
 
       if (path != nullptr)
